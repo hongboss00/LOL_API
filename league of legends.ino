2464e811a1e657227b3fdd620b8f9193fed69858
hongboss <leehongsg3@gmail.com>
2018-11-20 23:34:16 +0900

    2018-11-20-23:34:00

--- league of legends.ino
+++ league of legends.ino
@@ -10,6 +10,37 @@
 
 void setup()
 {
+	WiFi.begin("Home", "");
+
+
+	WiFiClient client;
+	const int httpPort = 80;
+	if (!client.connect("www.google.com", httpPort)) {
+		Serial.println("connection failed");
+		return;
+	}
+
+	// This will send the request to the server
+	client.print(String("GET / HTTP/1.1\r\n") +
+		String("Host: www.google.com\r\n") +
+		String("Connection: close\r\n\r\n"));
+	int repeatCounter = 0;
+	while (!client.available() && repeatCounter < 10) {
+		delay(1000);
+		Serial.println(".");
+		repeatCounter++;
+	}
+
+	String line;
+
+	int size = 0;
+	client.setNoDelay(false);
+	while (client.connected()) {
+		while ((size = client.available()) > 0) {
+			line = client.readString();
+			Serial.print(line);
+		}
+	}
 
 
 }
--- lol_api_client.cpp
+++ lol_api_client.cpp
@@ -11,18 +11,18 @@ void LOL_API::doupdate(String url) {
 	parser.setListener(this);
 	WiFiClient client;
 	const int httpPort = 80;
-	if (!client.connect("api.wunderground.com", httpPort)) {
+	if (!client.connect("kr.api.riotgames.com", httpPort)) {
 		Serial.println("connection failed");
-		return;          //void형태이므로 retturn값없이 그냥 함수를 종료함을 나타냄 즉 있으나 마나.
+		return;          //void형태이므로 retturn값없이 그냥 함수를 종료함을 나타냄 즉 있으나 마나
 	}
 
 	Serial.print("Requesting URL: ");
 	Serial.println(url);
 
 	// This will send the request to the server
-	client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-		"Host: api.wunderground.com\r\n" +
-		"Connection: close\r\n\r\n");
+	client.print(String("GET ") + url + " HTTP/1.1\r\n"); //+
+		// "Host: api.wunderground.com\r\n" +
+		//"Connection: close\r\n\r\n");
 	int retryCounter = 0;
 	while (!client.available()) {
 		delay(1000);
